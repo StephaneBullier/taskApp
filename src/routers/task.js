43415f35650fe5f0503c5903dcc2160d68adcec4
[StephaneBullier] <[sbullier@gmail.com]>
2020-10-05 17:14:27 +0200

    Add save command to update task

--- src/routers/task.js
+++ src/routers/task.js
@@ -68,10 +68,9 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every(update => allowedUpdates.includes(update))
-    const _id = req.params.id
 
     if (!isValidOperation) {
-        return res.status(400).send({error: 'Invalid updates'})
+        return res.status(400).send({ error: 'Invalid updates!' })
     }
 
     try {
@@ -82,7 +81,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
         }
 
         updates.forEach(update => task[update] = req.body[update])
-
+        await task.save()
         res.status(200).send(task)
     } catch (error) {
         res.status(400).send(error)
